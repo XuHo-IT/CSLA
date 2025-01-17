76391f712fb4ae36b38484b4b5ada7a4fb207eaa
Hoa Ngo Tran Xuan <hoantx4@fpt.com>
2025-01-17 07:56:18 +0000

    push all

---
+++ Start_CSLA/BO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csla;
+
+namespace Start_CSLA
+{
+    [Serializable]
+    public class BO:BusinessBase<BO>
+    {
+        public static readonly PropertyInfo<int>
+            IdProperty = RegisterProperty<int>(c => c.Id);
+        public int Id
+        {
+            get => GetProperty<int>(IdProperty);
+            set => SetProperty(IdProperty, value);
+        }
+        public static readonly PropertyInfo<string>
+            NameProperty = RegisterProperty<string>(c => c.Name);
+        public string Name
+        {
+            get=> GetProperty<string>(NameProperty);
+            set => SetProperty(NameProperty, value);
+
+        }
+        protected override void AddBusinessRules()
+        {
+            BusinessRules.AddRule(new Csla.Rules.CommonRules.Required(NameProperty));
+        }
+        [Fetch]
+        private void DataPortal_Fetch(int id)
+        {
+            Id = id;
+            Name = "Xuho";
+        }
+        [Create]
+        private void DataPortal_Create()
+        {
+            Id = -1;
+            Name = "New Person";
+        }
+    }
+}
---
+++ Start_CSLA/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using Csla;
+using Csla.Configuration;
+using IronPdf;
+using Microsoft.Extensions.DependencyInjection;
+using Start_CSLA;
+namespace csla_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddCsla();
+            var provider = services.BuildServiceProvider();
+           var  applicationContext = provider.GetService<ApplicationContext>();
+            var db = provider.GetRequiredService<IDataPortal<BO>>();
+            var newPerson = db.Create();
+            Console.WriteLine($"New person: {newPerson.Name}");
+            var exsitingPerson = db.Fetch(1);
+            Console.WriteLine($"Fetched person: {exsitingPerson.Name}");
+            //Generate PDF
+            var htmlContent = new StringBuilder();
+            htmlContent.Append("<h1>Person Details</h1>");
+            htmlContent.Append($"<p><strong>New Person:</strong>{newPerson.Name}</p>");
+            htmlContent.Append($"<p><strong>Fetched Person:</strong>{exsitingPerson.Name}</p>");
+            //Create PDF
+            var renderer = new ChromePdfRenderer();
+            var pdfDocument = renderer.RenderHtmlAsPdf(htmlContent.ToString());
+            //Save PDF
+            var outputPath = "PersonDetails.pdf";
+            pdfDocument.SaveAs(outputPath);
+            Console.WriteLine($"PDF generated and saved to {outputPath}");
+        }
+
+    }
+}
